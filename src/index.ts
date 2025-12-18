c89656c714419bca8237bc2fc2326aa99e09c395
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-18 01:19:29 -0800

    Upgrade modules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export { default as installPath } from './lib/installPath.ts';
 export { default as loadConfig, loadFileConfig } from './lib/loadConfig.ts';
 export { default as loadEnv, type LoadEnvOptions, type LoadEnvResult } from './lib/loadEnv.ts';
-export { default as wrapWorker } from './lib/wrapWorker.ts';
 export * from './types.ts';
